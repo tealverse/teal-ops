dc9caca1b56e20f61b1bac374a068b19aab6351c
Michael Bock <me@thought2.de>
2021-12-19 17:05:06 +0100

    Change server

--- server/network.nix
+++ server/network.nix
@@ -1,14 +1,14 @@
 {
-  network.description = "Web server";
+  network.description = "Build network";
 
-  webserver =
+  "teal-build" =
     { config, pkgs, modulesPath, ... }:
     {
       services.httpd.enable = true;
       services.httpd.adminAddr = "admin@teal.ooo";
 
       services.httpd.virtualHosts = {
-        "nix.teal.ooo" = {
+        "65.21.251.123" = {
           listen = [{ port = 80; }];
           documentRoot = "${pkgs.valgrind.doc}/share/doc/valgrind/html";
         };
@@ -17,12 +17,12 @@
       environment.systemPackages = [ pkgs.busybox ];
 
       boot.cleanTmpDir = true;
-      networking.hostName = "nix-teal";
+      networking.hostName = "teal-build";
 
       services.openssh.enable = true;
 
       users.users.root.openssh.authorizedKeys.keys = [
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDU3B+XTjn4zNzurQTU6A1arranEYprwl2jsJWWZfUMhNV5JMclYqSZhNonagsZkunElG+PBwvEkVdCHVs4FYHxH1jq7hMz+b7MyGjqF214FPVWTMe4sbBNU9qWH1TbLlJgrZCcr4of5JBomRnjJuGl5fcpQrTaJEBcRgCBbFpSP3e8EAD40lkKznB30tZkSb1jZMZokFZX5Ejc94ifOApfxPWKitzEVxTl2/fcbFllAPc5I/ZH58/i3nnRjjpsvBt5c1chyrYgF36hSOnRwwmyMn7q/KdmrRgfIdKbLFPPp82AF1DeZ+Ol5c5IsKZQsZ72ZnkEYRkaUxybEBjHwljp3FkkXho7uhs1q6xG+hUKp1G5hr5ABuFjWYGRVz53pPd/o6m5KKQn+m1oxoF3s2oCQdJJH8OXLpr46bzCybQ1w3OWrguhestjlf0gEUdBdwAY6bwW/T9C34twpLngcRTz6J0okUdOmH1JipR1rOYC8HtV/tn9E6GSuYQt0AGgTt0= nix-teal"
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC2iqS0q4FXoN6ugI5CjCbBkg0v0ZmKiAsf4sofqZDe2uJIFD6aPoEunxaerhGQf63wGJMXD256XUSwpINssE5JqslrWC2ED28zEATQA5wKE41Qif5nLQzIJ8Gxb2BZ1MNieaD3Zwj56kqf5Bj4kJLrDMG4+sB+yTsQnhnt68G9wkPErsl7RhW3bMfnC1UetE5OpvWJ21k4UxtBsYM5WdAUOxVKmbPOrCYVtrC8tntyrXCxKLBOBLXIlyrWA2p09MEu4+3z0t7KwHdlygsXwhe+TMDq1Te+Xs5yAOqxBwbxzVid6n3IxUxP28bpE6wpNOiR3TBs3heGs7b6BeZGqe/hKXa8FaNUDvKUj5UQEoiJ4VgpsuL4XJLnLvvDSNWg6vSEAgOa7veXcOxa++sF/aoAAQfoy1b30RVVsrdTRvN5Qbp+jRGk+Yahr3saWYKSZ6wvFn0u5mMeRW5MWyLk/rqB/pFmVB4RLoWLSx0G7fELBGlmjpRcZJjTYq25/PSLSLE= teal-build"
       ];
 
       imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
