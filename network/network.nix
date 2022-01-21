06072edc0e57121058b4a592190b1c3a3f4c5cbf
Michael Bock <mb@thought2.de>
2022-01-21 16:36:37 +0100

    Add flakes, fix hercules

--- network/network.nix
+++ network/network.nix
@@ -1,8 +1,10 @@
 {
   network.description = "Build network";
 
+  teal-hercules = import ./teal-hercules.nix;
+
   # "teal-build" = import ./teal-build.nix;
-  "teal-hydra" = import ./teal-hydra.nix;
+  # "teal-hydra" = import ./teal-hydra.nix;
 }
 
 
